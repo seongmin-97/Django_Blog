e4f1a9f5885ea8d7e0fc3619841781418f2f5d5f
seongmin-97 <seongmin.tech@gmail.com>
2021-01-31 04:48:37 +0900

    add blog

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class PostLV(ListView) :
     context_object_name = 'posts'
     paginate_by = 2
 
-class PostDV(detailView) :
+class PostDV(DetailView) :
     model = Post
 
 class PostAV(ArchiveIndexView) :
@@ -19,11 +19,15 @@ class PostAV(ArchiveIndexView) :
     date_field = 'modify_dt'
 
 class PostYAV(YearArchiveView) :
-    model = Postdate_field = 'modify_dt'
+    model = Post
     date_field = 'modify_dt'
     make_object_list = True
 
 class PostMAV(MonthArchiveView) :
+    model = Post
+    date_field = 'modify_dt'
+
+class PostDAV(DayArchiveView) :
     model = Post
     date_field = 'modify_dt'
 
